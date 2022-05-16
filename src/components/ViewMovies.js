072806c80ae9fd1a472f332c15b1c76cac587763
Mori Asano <moasano@cisco.com>
2022-05-16 12:52:09 -0400

    remove comment

--- src/components/ViewMovies.js
+++ src/components/ViewMovies.js
@@ -16,9 +16,6 @@ function ViewMovies({movies, filter}) {
             property = property.substr(1);
         }
         return function (a,b) {
-            /* next line works with strings and numbers, 
-             * and you may want to customize it to your needs
-             */
             var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
             return result * sortOrder;
         }
